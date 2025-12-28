41146fa79e92567c6855285d9dee98985b58ee05
Meysa <meysasouzza@gmail.com>
2025-12-28 13:48:17 -0300

    Atualizar calculadora em Python com tratamento de erros.

--- calculadora.py
+++ calculadora.py
@@ -1,29 +1,32 @@
 # Insira o número a ser calculado
-num1 = input('Digite o primeiro número: ')
-operacao = input('Digite o operador (* / - +): ')
-num2 = input('Digite o segundo número: ')
+try:
+  num1 = input ('Digite o número ')
+  operacao = input ('Digite o operador(* / - +): ')
+  num2 = input ('Digite o número ')
 
-# Conversão para número float
-num1_convert = float(num1)
-num2_convert = float(num2)
+  #Conversão pra número inteiro
+  num1_convert = float (num1)
+  num2_convert = float (num2)
 
-# Critérios para os operadores
-if operacao == "*":
+  #Critérios para os operadores
+  if operacao == "*":
     resultado = num1_convert * num2_convert
     print('Resultado:', resultado)
-
-elif operacao == "+":
+  if operacao == "+":
     resultado = num1_convert + num2_convert
     print('Resultado:', resultado)
-
-elif operacao == "-":
+  elif operacao == "-":
     resultado = num1_convert - num2_convert
     print('Resultado:', resultado)
+  elif operacao == "/":
+    if num2_convert != 0:
+      resultado = num1_convert / num2_convert 
+    else:
+      print('Erro divisão por zero')
+      exit ()
+  else:
+    print ('Resultado: ', resultado)
 
-elif operacao == "/":
-    resultado = num1_convert / num2_convert
-    print('Resultado:', resultado)
-
-else:
-    print('Operação Inválida!')
-
+#Caso o usuário digite um caractere inválido
+except: ValueError
+print('Digite apenas números válidos(use ponto para decimais).')
